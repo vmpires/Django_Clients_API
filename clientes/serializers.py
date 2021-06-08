8aa05659f54d9ce035df88c8068d0e2c293657f4
Victor Pires <vctprs@gmail.com>
2021-06-07 23:06:03 -0300

    New Verifications

---
+++ clientes/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from clientes.models import Cliente
+from .validators import *
+
+class ClienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cliente
+        fields = '__all__'
+    def validate(self,data):
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError({'cpf': 'O CPF deve ter 11 dígitos!'})
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({'nome': 'Não inclua números no nome!'})
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError({'rg': 'O RG deve ter 9 dígitos!'})
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({'celular': 'O celular deve ter ao menos 11 dígitos!'})
+        
+        return data
+    
---
+++ clientes/validators.py
@@ -0,0 +1,13 @@
+def cpf_valido(cpf):
+    return len(cpf) == 11
+
+def nome_valido(nome):
+    return nome.isalpha()
+
+def rg_valido(rg):
+    return len(rg) == 9
+
+def celular_valido(celular):
+    return len(celular) == 11
+    
+
